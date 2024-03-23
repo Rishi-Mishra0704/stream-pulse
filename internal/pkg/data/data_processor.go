5bedf914d97a15114a2e614be2469dfb17bee848
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2024-03-24 02:44:18 +0530

    add process and result function

--- internal/pkg/data/data_processor.go
+++ internal/pkg/data/data_processor.go
@@ -1,6 +1,9 @@
 package data
 
-import "sync"
+import (
+	"strings"
+	"sync"
+)
 
 type DataProcessor interface {
 	Process(data []byte) error
@@ -16,4 +19,26 @@ func NewRealTimeDataProcessor() *RealtimeDataProcessor {
 	return &RealtimeDataProcessor{
 		results: make(map[string]int),
 	}
+}
+
+func (p *RealtimeDataProcessor) Process(data string) {
+	words := strings.Fields(data)
+
+	p.mutex.Lock()
+	defer p.mutex.Unlock()
+	for _, word := range words {
+		p.results[word]++
+	}
+}
+
+func (p *RealtimeDataProcessor) Result() map[string]int {
+	p.mutex.Lock()
+	defer p.mutex.Unlock()
+
+	resultCopy := make(map[string]int)
+
+	for key, value := range p.results {
+		resultCopy[key] = value
+	}
+	return resultCopy
 }
