afaa2425f15802fa6207800329a3ceaa3a43b1d0
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2024-03-24 02:38:16 +0530

    add Processor interface

--- internal/pkg/data/data_processor.go
+++ internal/pkg/data/data_processor.go
@@ -1,1 +1,19 @@
-package pkg
+package data
+
+import "sync"
+
+type DataProcessor interface {
+	Process(data []byte) error
+	Results() map[string]int
+}
+
+type RealtimeDataProcessor struct {
+	results map[string]int
+	mutex   sync.Mutex
+}
+
+func NewRealTimeDataProcessor() *RealtimeDataProcessor {
+	return &RealtimeDataProcessor{
+		results: make(map[string]int),
+	}
+}
